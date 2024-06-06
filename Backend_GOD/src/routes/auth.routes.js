4a66c5fd5eadfe51d6b6c48341b9dcbd73c5cec3
Scharfcsh <amanadhikari2003@gmail.com>
2024-06-06 12:43:26 +0530

    cor added in routes

--- Backend_GOD/src/routes/auth.routes.js
+++ Backend_GOD/src/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
+import cors from "cors";
 import {Login, signup,Logout} from "../controllers/auth.controller.js";
 const router= express.Router();
 
 
-router.post('/login',Login);
+router.post('/login',cors(), Login);
 
-router.post('/signup',signup);
+router.post('/signup',cors(), signup);
 
-router.post('/logout',Logout);
+router.post('/logout',cors(), Logout);
 
 export default router;
