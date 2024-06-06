ac8f7acf3f3b0786672692c165e5757a583a9c1b
Scharfcsh <amanadhikari2003@gmail.com>
2024-06-06 12:19:25 +0530

    ..

--- Backend_GOD/index.js
+++ Backend_GOD/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 // Allow specific origin(s)
 app.use(cors({
-  origin: 'https://yourdeployedsite.com'
+  origin: 'http://localhost:5173'
 }));
 
 dotenv.config();
--- chatAppUI/vite.config.js
+++ chatAppUI/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
    
     proxy:{
       '/api':{
-        target:'http://localhost:8000'
+        target:'https://chirplebackend.vercel.app/'
       }
     }
   }
