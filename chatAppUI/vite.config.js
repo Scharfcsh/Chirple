35f2913bc189ace6f63c00d3f920f114ae18935f
Scharfcsh <amanadhikari2003@gmail.com>
2024-06-06 11:59:17 +0530

    okk

--- chatAppUI/vite.config.js
+++ chatAppUI/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
+   
     proxy:{
       '/api':{
-        target:'https://chirpleapp.vercel.app/'
+        target:'http://localhost:8000'
       }
     }
   }
